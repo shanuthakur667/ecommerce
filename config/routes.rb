fe99db4e420b5eeff592d39e65c2139686952393
yugansh-thakur <yugansh.kumar@reddoorz.com>
2020-04-28 01:17:56 +0530

    bootstrap, jquery-rails and font-awesome-sass gem, home page theam and es methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :products, only: [:index]
-  root 'products#index'
 
-  get "/*path", to: 'products#index'
+  resources :home, only: [:index]
+  resources :products, only: [:index]
+
+  root 'home#index'
+  get "/*path", to: 'home#index'
 end
