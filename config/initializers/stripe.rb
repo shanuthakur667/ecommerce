6647a58b1e40ab1656c06e144685c6d5daef1ca8
yugansh <shanuthakur667@gmail.com>
2020-05-09 02:29:56 +0530

    stripe syntax

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,5 +1,5 @@
 Rails.configuration.stripe = {
-  publishable_key: ENV['STRIPE_PUBLISHABLE_KEY']#Rails.application.secrets.stripe_publishable_key,
+  publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],#Rails.application.secrets.stripe_publishable_key,
   secret_key: ENV['STRIPE_PUBLISHABLE_KEY']#Rails.application.secrets.stripe_secret_key
 }
 
