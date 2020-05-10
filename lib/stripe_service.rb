fcd3c9f975d9b34fec61f91bfb31794a2c0253f6
yugansh <shanuthakur667@gmail.com>
2020-05-10 18:56:47 +0530

    add order

--- lib/stripe_service.rb
+++ lib/stripe_service.rb
@@ -1,6 +1,6 @@
 module StripeService
   Stripe.api_key = ENV['STRIPE_SECRET_kEY']
-  def create_or_find_customer user, params
+  def create_or_find_customer user, params, order
     unless user.present? && user.stripe_customer_id.present?
       delivary_detail = order.delivery_detail
       customer = Stripe::Customer.create({
@@ -19,7 +19,7 @@ module StripeService
   def create_charge(user, order, params)
     response = {}
     begin
-      customer_id = create_or_find_customer(user, params)
+      customer_id = create_or_find_customer(user, params, order)
       response = Stripe::Charge.create(customer: customer_id, amount: (order.total_price.to_i * 100), description: "product buy", currency: SYSREM_DEFAULT_CURRENCY)
     rescue Stripe::StripeError, Stripe::InvalidRequestError, Stripe::CardError => e
       response = {}
